252b6b2bfd1aaf236391e0426ce73f8cc57f6a7e
Xavier Francis <thisisxvr@gmail.com>
2020-01-10 19:42:02 +1300

    Implements robot bus functionality

--- src/bus.ts
+++ src/bus.ts
@@ -1,17 +1,68 @@
+
 export class Bus {
-  private _position: number;
+  private _north: number;
+  private _east: number;
+  private _direction: string;
+  readonly directions = ["NORTH", "EAST", "SOUTH", "WEST"];
 
-  constructor(position = 0) {
-    this._position = position;
+  constructor(x = 0, y = 0, facing = "NORTH") {
+    this._north = y;
+    this._east = x;
+    this._direction = facing;
   }
 
-  move_east() {
-    this._position++;
+  get north() { return this._north; }
+
+  get east() { return this._east; }
+
+  get direction() { return this._direction; }
+
+  move() {
+    switch (this._direction) {
+      case "NORTH":
+        this.move_north();
+        break;
+      case "EAST":
+        this.move_east();
+        break;
+      case "SOUTH":
+        this.move_south();
+        break;
+      case "WEST":
+        this.move_west();
+        break;
+      default:
+        break;
+    }
   }
 
-  move_west() {
-    this._position--;
+  move_north() { this._north++; }
+
+  move_east() { this._east++; }
+
+  move_south() { this._north--; }
+
+  move_west() { this._east--; }
+
+  turn_left() {
+    const facing = this._direction;
+    const newDirection = this.directions.slice(this.directions.indexOf(facing) - 1)[0];
+    this._direction = newDirection;
   }
 
-  get position() { return this._position; }
+  turn_right() {
+    const facing = this._direction;
+    const directions = this.directions;
+    directions.reverse();
+    const newDirection = directions.slice(directions.indexOf(facing) - 1)[0];
+    this._direction = newDirection;
+  }
+
+  report() {
+    return {
+      x: this._east,
+      y: this._north,
+      direction: this._direction
+    };
+  }
 }
--- tests/bus.test.ts
+++ tests/bus.test.ts
@@ -1,44 +1,189 @@
 import { Bus } from '../src/bus';
 
 
-describe('A Bus...', () => {
+describe('A Bus can move...', () => {
   let bus: Bus;
 
   beforeEach(() => {
     bus = new Bus();
   });
 
-  it('moves 3 spaces east', () => {
+  it('3 spaces east', () => {
     bus.move_east();
     bus.move_east();
     bus.move_east();
 
-    expect(bus.position).toEqual(3);
+    expect(bus.east).toEqual(3);
   });
 
-  it('moves 4 spaces east', () => {
+  it('4 spaces east', () => {
     bus.move_east();
     bus.move_east();
     bus.move_east();
     bus.move_east();
 
-    expect(bus.position).toEqual(4);
+    expect(bus.east).toEqual(4);
   });
 
-  it('moves 3 spaces west', () => {
+  it('3 spaces west', () => {
     bus.move_west();
     bus.move_west();
     bus.move_west();
 
-    expect(bus.position).toEqual(-3);
+    expect(bus.east).toEqual(-3);
   });
 
-  it('moves 4 spaces west', () => {
+  it('4 spaces west', () => {
     bus.move_west();
     bus.move_west();
     bus.move_west();
     bus.move_west();
 
-    expect(bus.position).toEqual(-4);
+    expect(bus.east).toEqual(-4);
+  });
+
+  it('3 spaces north', () => {
+    bus.move_north();
+    bus.move_north();
+    bus.move_north();
+
+    expect(bus.north).toEqual(3);
+  });
+
+  it('4 spaces north', () => {
+    bus.move_north();
+    bus.move_north();
+    bus.move_north();
+    bus.move_north();
+
+    expect(bus.north).toEqual(4);
+  });
+
+  it('3 spaces south', () => {
+    bus.move_south();
+    bus.move_south();
+    bus.move_south();
+
+    expect(bus.north).toEqual(-3);
+  });
+
+  it('4 spaces south', () => {
+    bus.move_south();
+    bus.move_south();
+    bus.move_south();
+    bus.move_south();
+
+    expect(bus.north).toEqual(-4);
+  });
+});
+
+describe('When facing North, A Bus...', () => {
+  let bus: Bus;
+
+  beforeEach(() => {
+    bus = new Bus(0, 0, "NORTH");
+  });
+
+  it('moves north', () => {
+    bus.move();
+    expect(bus.north).toEqual(1);
+  });
+
+  it('turns left to face West', () => {
+    bus.turn_left();
+    expect(bus.direction).toEqual("WEST");
+  });
+
+  it('turns right to face East', () => {
+    bus.turn_right();
+    expect(bus.direction).toEqual("EAST");
+  });
+});
+
+describe('When facing South, A Bus...', () => {
+  let bus: Bus;
+
+  beforeEach(() => {
+    bus = new Bus(0, 0, "SOUTH");
+  });
+
+  it('moves south', () => {
+    bus.move();
+    expect(bus.north).toEqual(-1);
+  });
+
+  it('turns left to face East', () => {
+    bus.turn_left();
+    expect(bus.direction).toEqual("EAST");
+  });
+
+  it('turns right to face West', () => {
+    bus.turn_right();
+    expect(bus.direction).toEqual("WEST");
+  });
+});
+
+describe('When facing East, A Bus...', () => {
+  let bus: Bus;
+
+  beforeEach(() => {
+    bus = new Bus(0, 0, "EAST");
+  });
+
+  it('moves east', () => {
+    bus.move();
+    expect(bus.east).toEqual(1);
+  });
+
+  it('turns left to face North', () => {
+    bus.turn_left();
+    expect(bus.direction).toEqual("NORTH");
+  });
+
+  it('turns right to face South', () => {
+    bus.turn_right();
+    expect(bus.direction).toEqual("SOUTH");
+  });
+});
+
+describe('When facing West, A Bus...', () => {
+  let bus: Bus;
+
+  beforeEach(() => {
+    bus = new Bus(0, 0, "WEST");
+  });
+
+  it('moves west', () => {
+    bus.move();
+    expect(bus.east).toEqual(-1);
+  });
+
+  it('turns left to face South', () => {
+    bus.turn_left();
+    expect(bus.direction).toEqual("SOUTH");
+  });
+
+  it('turns right to face North', () => {
+    bus.turn_right();
+    expect(bus.direction).toEqual("NORTH");
+  });
+});
+
+
+describe('A Bus.can report..', () => {
+  let bus: Bus;
+
+  beforeEach(() => {
+    bus = new Bus(0, 0, "NORTH");
+  });
+
+  it('its location and direction', () => {
+    const result = {
+      x: 0,
+      y: 0,
+      direction: "NORTH"
+    };
+
+    expect(bus.report()).toEqual(result);
   });
 });
